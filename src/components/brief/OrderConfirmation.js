327e6ea0bf4c87ac8d5b17e6511bf2ad54d487ac
didier <didiergonzalez79@outlook.es>
2023-09-23 01:59:18 -0300

    codeando

--- src/components/brief/OrderConfirmation.js
+++ src/components/brief/OrderConfirmation.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function OrderConfirmation({ purchasedData, cartItems }) {
+  // Verifica si purchasedData está definido antes de acceder a sus propiedades
+  if (!purchasedData) {
+    return <p>No hay datos de compra disponibles.</p>;
+  }
+
   return (
     <div>
       <h3>Compra Realizada</h3>
-      <p>Gracias por tu compra, {purchasedData.name}.</p>
-      <p>Número de teléfono: {purchasedData.phone}</p>
-      <p>Email: {purchasedData.email}</p>
+      <p>
+        Gracias por tu compra, {purchasedData.name || "Nombre no disponible"}.
+      </p>
+      <p>
+        Número de teléfono: {purchasedData.phone || "Teléfono no disponible"}
+      </p>
+      <p>Email: {purchasedData.email || "Email no disponible"}</p>
       <h4>Productos Comprados:</h4>
       <ul>
         {cartItems.map((item) => (
